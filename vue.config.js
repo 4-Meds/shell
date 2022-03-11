1f01768ba100658ce445c090aa558a7d6b0b9d3e
Kirill Voronin <yoursdearboy@gmail.com>
2022-03-12 02:14:50 +0300

    Set up module federation

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
 const { defineConfig } = require('@vue/cli-service')
+const { ModuleFederationPlugin } = require('webpack').container
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: 'http://localhost:8000/',
+  configureWebpack: {
+    plugins: [
+      new ModuleFederationPlugin({
+        name: 'shell',
+        filename: 'remoteEntry.js',
+        remotes: {
+          patients: 'patients@http://localhost:8001/remoteEntry.js'
+        },
+        shared: ['fhirclient', 'vue', 'bootstrap', 'bootstrap-vue']
+      })
+    ]
+  },
+  chainWebpack: config => {
+    // See https://github.com/vuejs/vue-cli/issues/6318
+    config.optimization.delete('splitChunks')
+  }
 })
